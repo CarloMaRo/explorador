15030cfa3de110b801494477ba5d4ddd3a447de8
CarloMaRo <camromeroro@unal.edu.co>
2025-12-09 17:01:00 -0500

    pruebas en la función graficador_espectro()

--- script_miscelaneo.py
+++ script_miscelaneo.py
@@ -1631,10 +1631,10 @@ def graficador_espectro(x,y,arr_pico, ax, limites_x, variable, unidad_tiempo):
   picos_en_frec   = [recorto_decimales(k) for k in x[arr_pico]]
   picos_de_tiempo = [recorto_decimales(k) for k in picos_de_tiempo]
 
-  ax.scatter(x[arr_pico], y[arr_pico], facecolor = 'r', label = 'Variable ' + str(variable) + '\nLas frencuencias son:' + str(picos_en_frec)+'  ( muestras / '+unidad_tiempo+' )\nY los tiempos son '+str(picos_de_tiempo)+' ('+unidad_tiempo+')')
+  ax.scatter(x[arr_pico], y[arr_pico]/max(y), facecolor = 'r', label = 'Variable ' + str(variable) + '\nLas frencuencias son:' + str(picos_en_frec)+'  ( muestras / '+unidad_tiempo+' )\nY los tiempos son '+str(picos_de_tiempo)+' ('+unidad_tiempo+')')
   #ax.plot(x, y)
-  print(x, y)
-  ax.vlines(x, ymin = 0, ymax = y, color = 'gray', linestyle='--', alpha=0.7)
+  print(x, y, y/max(y))
+  ax.vlines(x, ymin = 0, ymax = y/max(y), color = 'gray', linestyle='--', alpha=0.7)
   ax.set_yscale('log')
   ax.set_xlabel("Frecuencia")
   ax.set_ylabel("Potencia")
