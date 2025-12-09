5c11d3900cd64f95433c8b7b6b67752399f6828d
CarloMaRo <camromeroro@unal.edu.co>
2025-12-09 17:22:22 -0500

    se ajusta función grafica_espectros() para pintar con scatter todos los puntos, se resaltan los relevantes en color rojo, se pintan líneas punteadas desde el punto al eje hor y se grafican en escala de 0 a 1

--- script_miscelaneo.py
+++ script_miscelaneo.py
@@ -1634,7 +1634,7 @@ def graficador_espectro(x,y,arr_pico, ax, limites_x, variable, unidad_tiempo):
   ax.scatter(x, y/max(y), facecolor = 'gray')
   ax.scatter(x[arr_pico], y[arr_pico]/max(y), facecolor = 'r', label = 'Variable ' + str(variable) + '\nLas frencuencias son:' + str(picos_en_frec)+'  ( muestras / '+unidad_tiempo+' )\nY los tiempos son '+str(picos_de_tiempo)+' ('+unidad_tiempo+')')
   #ax.plot(x, y)
-  print(x, y, y/max(y))
+  #print(x, y, y/max(y))
   ax.vlines(x, ymin = 0, ymax = y/max(y), color = 'gray', linestyle='--', alpha=0.7)
   ax.set_yscale('log')
   ax.set_xlabel("Frecuencia")
